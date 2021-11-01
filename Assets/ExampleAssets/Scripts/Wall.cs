e2e27bf11581330ce4d214278082a086f34380de
Ravi1491 <ravi149185@gmail.com>
2021-11-01 14:54:40 +0530

    Add only new Scene

---
+++ Assets/ExampleAssets/Scripts/Wall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex +3);
+    }
+
+    // Update is called once per frame
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
